961cd53a1465447b76cc2d3257a8c67da068a60d
Sayantan Paul <69689739+legendsayantan@users.noreply.github.com>
2022-08-18 06:42:39 +0530

    Updated Instagram Auto Reactor

--- app/src/main/assets/InstaAutoReactor.js
+++ app/src/main/assets/InstaAutoReactor.js
@@ -2,7 +2,7 @@
 
 var userHasScrolled = false;
 var delay = 3000;//change this parameter,500+
-var likes = document.getElementsByClassName("wpO6b  ");
+var likes = document.getElementsByClassName('_abm0 _abl_');
 window.onscroll = async function (e)
 {
     if(!userHasScrolled){
@@ -14,6 +14,6 @@ window.onscroll = async function (e)
 }
 async function main(){
 for(var i = 0;i<likes.length;i++){
-    var detect = likes[i].getElementsByClassName("_8-yf5 ");
+    var detect = likes[i].getElementsByClassName("_ab6-");
     if(detect[0].getAttribute('aria-label')=='Like')likes[i].click();
 }}
